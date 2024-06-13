744aacfd6e15849eb3f8250fd8f7393f9f918060
Amr Fawaz <amr.abdelhakim.fawaz+75@gmail.com>
2024-06-14 01:55:00 +0300

    Initial Commit

---
+++ PopularMovies/PopularMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularMoviesApp.swift
+//  PopularMovies
+//
+//  Created by AmrFawaz on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct PopularMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
